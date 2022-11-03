84b9edce4104c00ae65d245d71c0d1ebe1d2210c
Shengjun（Eric）Ma <61225605+SericMa@users.noreply.github.com>
2022-11-04 08:28:39 +1300

    Update and rename plot.py to plotTheCoordinates.py

---
+++ plotTheCoordinates.py
@@ -0,0 +1,65 @@
+"""
+--* coding: utf-8 *--
+@Author:Eric Ma
+@Year: 2022
+@python version: python 3.8
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+# read the csv file
+data = pd.read_csv('daniel.csv')
+
+# drop out the columns called 'thermistor' and 'time' and "first column"
+data = data.iloc[:, 1:69].drop(columns=['time', 'thermistor'])
+
+# generate a DataFrame called 'coordinates' which will present the coordinates of location
+x = data.iloc[:, 64].values
+y = data.iloc[:, 65].values
+d = {'x': x, 'y': y}
+coordinates = pd.DataFrame(d)
+
+"""
+* plot the coordinates included "boundary"
+* spilt the coordinates by "in/out of coverage"
+* "i" means "in coverage"
+* "o" means "out of coverage"
+"""
+
+# splitting
+# i = coordinates[(coordinates.x <= -1.44) | (coordinates.x >= 1.44) | (coordinates.y >= 3.94) | (coordinates.y <= 1.1)]
+# o = coordinates[(coordinates.x > -1.44) & (coordinates.x < 1.44) & (coordinates.y < 3.94) & (coordinates.y > 1.1)]
+
+
+i = coordinates[(coordinates.x <= -0.20) | (coordinates.x >= 0.20) | (coordinates.y >= 2.9) | (coordinates.y <= 1.1)]
+o = coordinates[(coordinates.x > -0.20) & (coordinates.x < 0.20) & (coordinates.y < 2.9) & (coordinates.y > 1.1)]
+
+# plot the chart
+
+fig1, ax1 = plt.subplots()
+ax1.scatter(i.x, i.y, color="black")
+ax1.scatter(o.x, o.y, color="green")
+ax1.set_ylim(ymin=-1)
+ax1.set_xlim(xmin=-4)
+ax1.add_patch(
+    patches.Rectangle(
+        (-1.44, 1.1),  # Coordinate(x ,y)
+        2.88,  # width
+        2.88,  # height
+        fill=None,  # fill color
+        color="orange",  # line color
+    )
+)
+
+ax1.add_patch(patches.Rectangle(
+    (-0.20, 1.1),  # Coordinate(x ,y)
+    0.4,  # width
+    1.8,  # height
+    fill=None,  # fill color
+    color="red",  # line color
+))
+
+plt.show()
